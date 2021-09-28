22cdaf2c8e18292dbb65de1ccae5124a71008ca0
ducvu <vuduc2701@gmail.com>
2021-09-28 15:15:43 +0700

    ckeditor

--- LapTrinhQuanLy/App_Start/BundleConfig.cs
+++ LapTrinhQuanLy/App_Start/BundleConfig.cs
@@ -19,6 +19,9 @@ namespace LapTrinhQuanLy
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/ckeditor").Include(
+                      "~/Scripts/ckeditor/ckeditor.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
--- LapTrinhQuanLy/Models/Person.cs
+++ LapTrinhQuanLy/Models/Person.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-
+using System.Web.Mvc;
 
 namespace LapTrinhQuanLy.Models
 {
@@ -16,6 +16,7 @@ namespace LapTrinhQuanLy.Models
             [Key]
             [Required(ErrorMessage = "ID Khong duoc de trong !")]
             public string PersonID { get; set; }
+            [AllowHtml]
             public string PersonName { get; set; }
         }
     
